2af0e23f8da5f08dc8fcb26670bd9a1c2e8a090c
Vikrant Gupta <vikrantgupta01@gmail.com>
2020-10-21 16:07:21 +1100

    initl commit

---
+++ src/main/java/com/javainuse/controller/SampleDataController.java
@@ -0,0 +1,25 @@
+package com.javainuse.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class SampleDataController {
+
+    @RequestMapping({"/sample"})
+    public ResponseEntity<Map<String, String>> listAllHeaders(
+            @RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+            System.out.println(String.format("Header '%s' = %s", key, value));
+        });
+
+
+        return new ResponseEntity<Map<String, String>>(headers, HttpStatus.OK);
+    }
+
+}
